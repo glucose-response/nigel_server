e635563db7538d55e9947941b3d5f077c26e8e9e
Tian <Tian.pan2003@gmail.com>
2023-12-19 17:11:30 +0000

    adding baby database

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+# Ref 1: https://flask-pymongo.readthedocs.io/en/latest/ 
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
+# # Load config from .env file
+# load_dotenv()
+# app.config['MONGO_URI'] = os.environ['MONGODB_URI']
+# mongo = PyMongo(app)
+
+# # app.config['MONGO_URI'] = "mongodb+srv://Tpan:476PaZI6ygSnwFCm@nigel.zqqhrjd.mongodb.net/?retryWrites=true&w=majority"
+# # mongo = PyMongo(app)
+# print(f"Mongo: {mongo}")
+# print(f"DB: {mongo.db}")
 
 @app.route("/")
 def index():
     return "Hello this is the main page"
+
+# @app.route("/profiles")
+# def baby_profiles():
+#     try:
+#         # Fetch all documents from the 'BabyProfiles' collection
+#         babies = mongo.Nigel.BabyProfiles.find({})
+
+#         # Convert MongoDB cursor to a list for easy printing
+#         baby_list = list(babies)
+
+#         # Print each document
+#         for baby in baby_list:
+#             print(baby)
+
+#         return jsonify({'profiles': baby_list})
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#         return jsonify({'error': f'An error occurred while fetching profiles: {str(e)}'})
 
 @app.route("/testing")
 def testing():
